7c805d9b0df35fe05ceeaab7ce2caeec4b8889b6
Beato <cristianobeato_dm@hotmail.com>
2025-07-23 23:19:45 -0300

    include crvkLib

--- neo/renderer/renderer_common.h
+++ neo/renderer/renderer_common.h
@@ -29,6 +29,8 @@ If you have questions concerning this license or the applicable additional terms
 #ifndef __TR_LOCAL_H__
 #define __TR_LOCAL_H__
 
+#include <crvkCore.hpp>
+
 // BEATO Begin: pipeline implementation
 class idRenderWorldLocal;
 #include "RenderMatrix.h"
